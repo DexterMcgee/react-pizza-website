fbb7a8a39b24660b5ebe20e618c89e2d051fa168
victor <victorzemail@gmail.com>
2022-06-08 21:53:13 -0500

    Front Page Pizza Site

---
+++ src/components/Products/data.js
@@ -0,0 +1,59 @@
+import product1 from "../../images/product-1.jpg";
+import product2 from "../../images/product-2.jpg";
+import product3 from "../../images/product-3.jpg";
+import sweet1 from "../../images/sweet-1.jpg";
+import sweet2 from "../../images/sweet-2.jpg";
+import sweet3 from "../../images/sweet-3.jpg";
+
+export const productData = [
+  {
+    img: product1,
+    alt: "Pizza",
+    name: "Supreme Pizza",
+    desc: "Marinara sauce, basil, Sausage, Roma tomatoes, black olive, and pesto",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product2,
+    alt: "Pizza",
+    name: "Hawaiian Supreme",
+    desc: "Barbecue sauce, Chicken, Pineapple, Red Onion, and Cilantro",
+    price: "$24.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product3,
+    alt: "Pizza",
+    name: "Ham and Veg",
+    desc: "Marinara sauce, Canadian bacon, Classic Thick Crust, and Arugula",
+    price: "$16.99",
+    button: "Add to Cart",
+  },
+];
+export const productDataTwo = [
+  {
+    img: sweet1,
+    alt: "Sweet",
+    name: "Mint Sundae",
+    desc: "Chocolate Mousse, Mint Oreo, Vanilla Ice Cream, all topped with Chocolate Sprinkles and a Decadent Chocolate Syrup",
+    price: "$9.99",
+    button: "Add to Cart",
+  },
+  {
+    img: sweet2,
+    alt: "Sweet",
+    name: "Frosted Donuts",
+    desc: "Fluffy donuts with your choice of frosting, topped with Sprinkles and Chocolate Drizzle",
+    price: "$2.99 each",
+    button: "Add to Cart",
+  },
+  {
+    img: sweet3,
+    alt: "Sweet",
+    name: "The Avalanche",
+    desc: "Vanilla Ice Cream over Hot Chocolate Cake drenched in Caramel and Chocolate Syrup and finished with a wonderfully flakey Pirouette ",
+    price: "$10.99",
+    button: "Add to Cart",
+  },
+];
